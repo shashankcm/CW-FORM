2b3838acd6ecd0f3d0ec420d7cffef6e0c3f0495
unknown <shashankcm90@gmail.com>
2019-02-27 11:28:33 -0600

    Initial Commit

---
+++ src/variables/sampleFormData.jsx
@@ -0,0 +1,351 @@
+const totalListOfRefferals = [
+  {
+    _id: "5c6c88459fedd450845b3d89",
+    forminstance: {
+      formID: "100",
+      accountID: "100000",
+      data: {
+        application: {
+          sections: [
+            {
+              section: {
+                firstname: "chandan",
+                lastname: "kumar"
+              }
+            },
+            {
+              section2: {
+                address: "100 Canal Pt Blv"
+              }
+            },
+            {
+              section3: {
+                income: "100000",
+                wageType: "w2"
+              }
+            }
+          ]
+        }
+      }
+    },
+    __v: 0
+  },
+  {
+    _id: "5c6c88629fedd450845b3d8a",
+    forminstance: {
+      formID: "100",
+      accountID: "100000",
+      data: {
+        application: {
+          sections: [
+            {
+              section: {
+                firstname: "chandan",
+                lastname: "kumar"
+              }
+            },
+            {
+              section2: {
+                address: "100 Canal Pt Blv"
+              }
+            },
+            {
+              section3: {
+                income: "100000",
+                wageType: "w2"
+              }
+            }
+          ]
+        }
+      }
+    },
+    __v: 0
+  },
+  {
+    _id: "5c6c887b68470117fc2c2722",
+    forminstance: {
+      formID: "100",
+      accountID: "100000",
+      data: {
+        application: {
+          sections: [
+            {
+              section: {
+                firstname: "chandan",
+                lastname: "kumar"
+              }
+            },
+            {
+              section2: {
+                address: "100 Canal Pt Blv"
+              }
+            },
+            {
+              section3: {
+                income: "100000",
+                wageType: "w2"
+              }
+            }
+          ]
+        }
+      },
+      audit: {
+        createdBy: "chandan",
+        dateCreated: "02/19/2019 17:51:39",
+        updatedBy: "null",
+        dateModified: "null"
+      }
+    },
+    __v: 0
+  },
+  {
+    _id: "5c6c89611c9b2058ccdeeeab",
+    forminstance: {
+      formID: "100",
+      accountID: "100000",
+      data: {
+        application: {
+          sections: [
+            {
+              section: {
+                firstname: "chandan",
+                lastname: "kumar"
+              }
+            },
+            {
+              section2: {
+                address: "100 Canal Pt Blv"
+              }
+            },
+            {
+              section3: {
+                income: "100000",
+                wageType: "w2"
+              }
+            }
+          ]
+        }
+      },
+      audit: {
+        createdBy: "chandan",
+        dateCreated: "02/19/2019 17:55:29",
+        updatedBy: "null",
+        dateModified: "null"
+      }
+    },
+    __v: 0
+  },
+  {
+    _id: "5c6c89832f976c2e4480d4f1",
+    forminstance: {
+      formID: "100",
+      accountID: "100000",
+      data: {
+        application: {
+          sections: [
+            {
+              section: {
+                firstname: "jam",
+                lastname: "kk"
+              }
+            },
+            {
+              section2: {
+                address: "100 Canal Pt Blv111"
+              }
+            },
+            {
+              section3: {
+                income: "100000",
+                wageType: "w2"
+              }
+            }
+          ]
+        }
+      },
+      audit: {
+        createdBy: "chandan",
+        dateCreated: "02/19/2019 17:56:03",
+        updatedBy: "sam",
+        dateModified: "02/21/2019 11:14:46"
+      }
+    },
+    __v: 0
+  },
+  {
+    _id: "5c6ef3a7f9ee0b30e04c0a78",
+    forminstance: {
+      formID: "5c6b3704310ef355a8527b46",
+      accountID: "100000",
+      data: {
+        application: {
+          sections: [
+            {
+              section: {
+                firstname: "abid api",
+                lastname: "khan"
+              }
+            },
+            {
+              section2: {
+                address: "trenton Canal Pt Blv"
+              }
+            },
+            {
+              section3: {
+                income: "12",
+                wageType: "w2"
+              }
+            }
+          ]
+        }
+      },
+      audit: {
+        createdBy: "chandankumar",
+        dateCreated: "02/21/2019 13:53:27",
+        updatedBy: "null",
+        dateModified: "null"
+      }
+    },
+    __v: 0
+  },
+  {
+    _id: "5c6ef43450b5373ad42fd047",
+    forminstance: {
+      formID: "5c6b3704310ef355a8527b46",
+      accountID: "100000",
+      data: {
+        application: {
+          sections: [
+            {
+              section: {
+                firstname: "abid",
+                lastname: "khan"
+              }
+            },
+            {
+              section2: {
+                address: "trenton Canal Pt Blv"
+              }
+            },
+            {
+              section3: {
+                income: "12",
+                wageType: "w2"
+              }
+            }
+          ]
+        }
+      },
+      audit: {
+        createdBy: "chandankumar",
+        dateCreated: "02/21/2019 13:55:48",
+        updatedBy: "null",
+        dateModified: "null"
+      }
+    },
+    __v: 0
+  },
+  {
+    _id: "5c6ef8091210741c4cbc675d",
+    forminstance: {
+      formID: "5c6b3704310ef355a8527b46",
+      accountID: "100000",
+      data: {
+        application: {
+          sections: [
+            {
+              section: {
+                firstname: "abid",
+                lastname: "khan"
+              }
+            },
+            {
+              section2: {
+                address: "trenton Canal Pt Blv"
+              }
+            },
+            {
+              section3: {
+                income: "12",
+                wageType: "w2"
+              }
+            }
+          ]
+        }
+      },
+      audit: {
+        createdBy: "mack",
+        dateCreated: "02/21/2019 14:12:09",
+        updatedBy: "null",
+        dateModified: "null"
+      }
+    },
+    __v: 0
+  },
+  {
+    _id: "5c6f0cd35b4e5a03f086182b",
+    forminstance: {
+      formID: "100",
+      accountID: "100000",
+      data: {
+        application: {
+          sections: [
+            {
+              section: {
+                firstname: "chandank1",
+                lastname: "kumar"
+              }
+            },
+            {
+              section2: {
+                address: "100 Canal Pt Blv"
+              }
+            },
+            {
+              section3: {
+                income: "100000",
+                wageType: "w2"
+              }
+            }
+          ]
+        }
+      },
+      audit: {
+        createdBy: "tywin user",
+        dateCreated: "02/21/2019 15:40:51",
+        updatedBy: null,
+        dateModified: "02/22/2019 15:29:14"
+      }
+    },
+    __v: 0
+  }
+];
+
+const dataTable = {
+  headerRow: ["ID", "Form ID", "Form Name", "Created By", "Created Date"],
+  footerRow: ["ID", "Form ID", "Form Name", "Created By", "Created Date"],
+  dataRows: []
+};
+
+totalListOfRefferals.forEach(ref => {
+  //console.log(ref);
+  let data = [],
+    _id,
+    form_id,
+    form_Name,
+    created_By,
+    created_Date;
+  _id = ref._id;
+  form_id = ref.forminstance.formID;
+  form_Name = "Form Name";
+  created_By = ref.forminstance.audit ? ref.forminstance.audit.createdBy : "";
+  created_Date = ref.forminstance.audit
+    ? ref.forminstance.audit.dateCreated
+    : "";
+
+  data.push(_id, form_id, form_Name, created_By, created_Date);
+  dataTable.dataRows.push(data);
+});
+
+export { dataTable };
